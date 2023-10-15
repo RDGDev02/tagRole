72e4c04e66b8ee17344ac3df7935daa2a724771f
Jasper Gozum <gozumjasper@gmail.com>
2023-10-16 04:08:26 +0800

    updated tag Checker

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const sessionSecret = process.env.SESSION_SECRET;
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
